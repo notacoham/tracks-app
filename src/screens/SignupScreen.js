e3be1493e90b9f12a6a953c7c47a44e4dc0c5dee
notacoham <alexcottam12@gmail.com>
2025-07-02 22:21:13 -0600

    add basic styling and configuration to Signup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,40 @@
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Text, Input, Button } from "@rneui/themed";
+import Spacer from "./components/Spacer";
+import { useLayoutEffect } from "react";
 
 const SignupScreen = () => {
   const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      header: null,
+    });
+  });
+
   return (
-    <>
-      <Text h3>Sign Up for Tracker</Text>
-    </>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Spacer />
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 125,
+  },
+});
 
 export default SignupScreen;
