1dd4c22a76c49cfa1e1f6f234191ac3b63a98c12
notacoham <alexcottam12@gmail.com>
2025-07-03 22:25:38 -0600

    add location tracking permission request function

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,35 @@
 import { Text } from "@rneui/themed";
 import { SafeAreaView, StyleSheet } from "react-native";
 import Map from "../components/Map";
+import { requestForegroundPermissionsAsync } from "expo-location";
+import { useEffect, useState } from "react";
+import Spacer from "../components/Spacer";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const granted = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted.");
+      }
+    } catch (error) {
+      setErr(error);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text h3>Create a Track</Text>
-      <Map />
+      <Spacer>
+        <Map />
+      </Spacer>
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
